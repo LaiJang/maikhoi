da77c60ee7f60b08af45d202b94b01f6163e07ca
LaiJang <LaiJang@users.noreply.github.com>
2018-07-22 21:48:29 +0700

    Add files via upload

---
+++ changeGimg.js
@@ -0,0 +1,19 @@
+function readyFn( jQuery ) {
+var x = document.getElementsByClassName("ty-img");
+var i;
+for (i = 0; i < x.length; i++) {
+x[i].style.backgroundImage = x[i].style.backgroundImage.replace(/[0-9]+.bp.blogspot.com/,"lh4.ggpht.com").replace("s1600","s600");
+}
+
+var y = document.getElementsByClassName("yard-img");
+var i;
+for (i = 0; i < y.length; i++) {
+y[i].style.backgroundImage = y[i].style.backgroundImage.replace(/[0-9]+.bp.blogspot.com/,"lh4.ggpht.com").replace("s1600","s500");
+}
+
+var z = document.getElementsByClassName("post-of-mine");
+var i;
+for (i = 0; i < z.length; i++) {
+z[i].style.backgroundImage = z[i].style.backgroundImage.replace(/[0-9]+.bp.blogspot.com/,"lh4.ggpht.com").replace("s1600","s500");
+}
+}
